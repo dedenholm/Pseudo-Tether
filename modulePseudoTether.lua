e4954005baed532d1c03ffa86cfca6cdd79e47ab
Daniel Rognskog Edenholm <dedenholm@gmail.com>
2024-12-16 17:57:17 +0100

    various edits

--- modulePseudoTether.lua
+++ modulePseudoTether.lua
@@ -32,15 +32,20 @@ https://www.darktable.org/lua-api/index.html.php#darktable_new_widget
 local dd = require "lib/dtutils.debug"
 local dt = require "darktable"
 local du = require "lib/dtutils"
+local ds = require "lib/dtutils.string"
 local df = require "lib/dtutils.file"
 local inotify = require "inotify"
 local handle = inotify.init { blocking = false }
+
 du.check_min_api_version("7.0.0", "moduleExample")
-local handle_returned =false
 local sleep = dt.control.sleep
 -- https://www.darktable.org/lua-api/index.html#darktable_gettext
-local gettext = dt.gettext.gettext 
-
+local gettext = dt.gettext.gettext
+local dsprobe =table.unpack(ds)
+dd.dprint (dsprobe)
+print("a")
+dd.dprint(ds.build_substitute_list)
+print("b")
 local watch_active = false
 dt.gettext.bindtextdomain("moduleExample", dt.configuration.config_dir .."/lua/locale/")
 
@@ -53,8 +58,8 @@ local script_data = {}
 
 script_data.metadata = {
   name = "Pseudo Tether",
-  purpose = _("Automatically import files from folder"),
-  author = "Daniel Rognkskog Edenholm",
+  purpose = _("Automatically import files added to folder"),
+  author = "Daniel Rognskog Edenholm",
   help = ""
 }
 
@@ -77,7 +82,7 @@ mE.module_installed = false  -- keep track of whether the module is module_insta
 ]]
 
  local function table_contains(table, value)
-  
+
   for i in ipairs(table) do
     if (table[i] == value) then
       return true
@@ -158,6 +163,7 @@ dt.preferences.register("Pseudo_Tether",
                           "add a custom extension to import",
                           ".IIQ"
                         )
+if dt.preferences.read("Pseudo_Tether", "session_counter", "integer") <= 0 then print("no session counter") dt.preferences.write("Pseudo_Tether", "session_counter", "integer", "0000") else print("session_counter: " .. dt.preferences.read("Pseudo_Tether", "session_counter", "integer") )end
 
 
 local ext_table = {"jpeg","nef","cr2", "dng","tiff"}
@@ -172,31 +178,32 @@ local ext_watched_extensions = {}
           if ext_table[ext]  == "jpeg" then
             table.insert(ext_watched_extensions, "jpg")
           end
-
           if ext_table[ext] == "tiff" then
             table.insert(ext_watched_extensions, "tif")
           end
-
-
         end
   end
+
 table.insert(ext_watched_extensions, string.lower(dt.preferences.read("Pseudo_Tether", "ext_custom", "string")))
 
+print(table.unpack(ext_watched_extensions))
 local default_ingest_directory = dt.preferences.read("Pseudo_Tether",
 "default_ingest_directory",
 "directory")
 
 print(dt.films[1].path .. " path to film #1!")
 
+
 local function install_module()
   if not mE.module_installed then
     -- https://www.darktable.org/lua-api/index.html#darktable_register_lib
     dt.register_lib(
-      "exampleModule",     -- Module name
-      "exampleModule",     -- name
+      "PseudoTether",     -- Module name
+      "PseudoTether",     -- name
       true,                -- expandable
       false,               -- resetable
-      {[dt.gui.views.lighttable] = {"DT_UI_CONTAINER_PANEL_RIGHT_CENTER", 100}},   -- containers
+
+      {[dt.gui.views.lighttable] = {"DT_UI_CONTAINER_PANEL_RIGHT_CENTER", 100},[dt.gui.views.darkroom] = {"DT_UI_CONTAINER_PANEL_LEFT_CENTER", 100}},   -- containers
       -- https://www.darktable.org/lua-api/types_lua_box.html
       dt.new_widget("box") -- widget
       {
@@ -206,6 +213,7 @@ local function install_module()
       nil,-- view_enter
       nil -- view_leave
     )
+
     mE.module_installed = true
   end
 end
@@ -213,25 +221,25 @@ local default_destination_directory = dt.preferences.read("Pseudo_Tether","defau
   "directory"
 )
 
-print ("dimpf" .. default_destination_directory)-- script_manager integration to allow a script to be removed
+print ("dimpf" .. default_destination_directory)
 
+-- script_manager integration to allow a script to be removed
 -- without restarting darktable
+
 local function destroy()
-    dt.gui.libs["exampleModule"].visible = false -- we haven't figured out how to destroy it yet, so we hide it for now
+    dt.gui.libs["PseudoTether"].visible = false -- we haven't figured out how to destroy it yet, so we hide it for now
 end
 
 local function restart()
-    dt.gui.libs["exampleModule"].visible = true -- the user wants to use it again, so we just make it visible and it shows up in the UI
+    dt.gui.libs["PseudoTether"].visible = true -- the user wants to use it again, so we just make it visible and it shows up in the UI
 end
 
--- https://www.darktable.org/lua-api/types_lua_check_button.html
 
 
--- https://www.darktable.org/lua-api/types_lua_entry.html
-local jobcode = dt.new_widget("entry")
+local session_title = dt.new_widget("entry")
 {
-    text = "jobcode",
-    placeholder = _("jobcode"),
+    text = "Capture Session",
+    placeholder = _("Capture Session"),
     is_password = false,
     editable = true,
     tooltip = _("name of the filmroll you want to import to"),
@@ -250,48 +258,74 @@ local ingest_directory = dt.new_widget("file_chooser_button")
     value = default_ingest_directory,                       -- The currently selected file
     is_directory = true              -- True if the file chooser button only allows directories to be selecte
 }
+local session_counter = dt.new_widget("label")
+      session_counter.label = dt.preferences.read("Pseudo_Tether", "session_counter", "integer")
+
 
 local label = dt.new_widget("label")
 label.label = _("my label") -- This is an alternative way to the "{}" syntax to set a property 
 --local separator = dt.new_widget("separator"){}
+local separator = dt.new_widget("separator")
+separator.orientation = "vertical"
+local prepend_date =dt.new_widget("check_button"){label = _("Add datecode prefix"),
+visible =true,
+value = true}
 
-local button3_toggle =false
-local function button3_clicked(handle_read_cancel)
-  if button3_toggle == true then
-      button3_toggle = false
-  else
-      button3_toggle = true 
-  end
+local ingest_directory_box = dt.new_widget("box"){
+                                    orientation ="horizontal",
+                                    dt.new_widget("label"){label="Watched Directory: ", halign ="start"},
+                                    ingest_directory
+}
 
-  local watch_dir = ingest_directory.value
-
-  print ("button3 clicked")
-
-  dd.dprint(dt.gui.libs.copy_history)
- 
-  -- handle_read_cancel(watch_dir)
-end
-
-
+local destination_directory_box =dt.new_widget("box"){
+                                    orientation ="horizontal",
+                                    dt.new_widget("label"){label="Destination Directory: ", halign ="start"},
+                                    destination_directory
+}
 local move_files = dt.new_widget("check_button")
         {
           label = _("move to library"),
           visible = true,
           value = true
 }
+local reset_session_counter = dt.new_widget("button")
+        {label = "Reset session counter"}
+local move_files_options = dt.new_widget("box"){orientation = "horizontal",
+prepend_date, reset_session_counter}
+local jobcode_box = dt.new_widget("box"){
+                                    orientation ="horizontal",
+                                    dt.new_widget("label"){label = "Jobcode:",halign ="start"},
+                                    session_title,
+                                    session_counter
+}
+local function jobcode(image, sequence)
+  print("function jobcode" .. sequence)
+  print(session_title.text)
+  print(session_counter.label .. " counter")
+  local session = session_title.text .. " " .. session_counter.label
+  if prepend_date.value == true then session = "$(YEAR)$(MONTH)$(DAY)".. session end
+  ds.build_substitute_list(image, sequence, session)
 
-local function handle_read_cancel(watch_dir)
-  local cancel_file = watch_dir .. '/cancel_file'
-  local file = io.open(cancel_file, "w")
+  local jobcode_substituted =  ds.substitute_list(session)
 
-
-  if file == nil then print("error writing file") return
-  else file:close() end
-
-  print(cancel_file .."written")
-  os.remove(cancel_file)
-  print(cancel_file .."deleted")
+  --local jobcode_substituted = ds.substitute_list(image, sequence, session_title.text ..  " " .. session_counter.label)
+  --jobcode = ds.substitute(image, sequence, session_title.text .. " " .. session_counter.label)
+  print(jobcode_substituted)
+  return jobcode_substituted
 end
+
+local function move_files_extra_options()
+  if move_files.value == false then
+    destination_directory_box.visible = false
+    jobcode_box.visible = false
+    move_files_options.visible =false
+  elseif move_files.value == true then
+    destination_directory_box.visible = true
+    move_files_options.visible =true
+    jobcode_box.visible = true
+  end
+end
+
 
 local function import_to_library(file)
     --file = df.sanitize_filename(file)
@@ -307,33 +341,36 @@ local function import_to_library(file)
     local imported_image = dt.database.import(file)
     --dd.dprint(imported_image)
     return imported_image
+
     else
       print( file.. " with filetype " .. filetype .. " is not wanted" )
     end
     end
-    print ("Jobcode" .. jobcode.text)
+    return 0
 
-    return imported_image
 end
 
 
-local function sort_in_library(imported_image)
- dd.dprint(imported_image)
+local function sort_in_library(imported_image, sequence)
+  dd.dprint(imported_image)
+  print ("sequence " .. sequence)
 
   if df.check_if_file_exists(destination_directory.value) == true then
 
 
     print(destination_directory.value .. " exists")
-
-    local final_directory = destination_directory.value .. "/" .. jobcode.text
+    print ("calculated jobcode: " .. jobcode(imported_image, sequence))
+    local final_directory = destination_directory.value .. "/" .. jobcode(imported_image, sequence)
 
     local  film_already_exists = search_film(final_directory)
     print(film_already_exists)
     if film_already_exists == 0 then
 
       print("film_already_exists == 0")
-
-      if df.check_if_file_exists(final_directory) ==false then print ("making dir") df.mkdir(final_directory) end
+      if df.check_if_file_exists(final_directory) == false then print ("making dir") df.mkdir(df.sanitize_filename(final_directory))
+        if df.check_if_file_exists(final_directory) == false then print("failed creating " .. final_directory) return end end
+      print("final dirextory " .. final_directory)
+      print("final Directory sanitized" .. df.sanitize_filename(final_directory))
       print("create film")
       local new_film = dt.films.new(final_directory)
       print (new_film.path .. "newfilm")
@@ -341,35 +378,40 @@ local function sort_in_library(imported_image)
       print ("move image")
       dt.database.move_image(new_film, imported_image)
 
+
     else
-       
+
       print("film already exists. adding image")
-     
       dt.database.move_image(imported_image, dt.films[film_already_exists])
       print ("image added to film ".. final_directory)
       print ("image moved to" .. dt.films[film_already_exists].path)
 
      end
-
   end
 end
 
 
 
-local function handle_read(watch_dir, button2)
---local event= handle:read()
+local function handle_read(watch_dir)
   print(watch_dir)
   watch_active= true
-  --dd.dprint(event)
+
+  local dt_message = "Files added to '" .. watch_dir .. "' will be imported"
+  if move_files.value == true then dt_message = dt_message ..  " and moved to '" .. destination_directory.value .. "/" end -- .. jobcode() .. "'"end
+
+  dt.print("Capture Session Started")
+  dt.print(dt_message)
+
   print(inotify.IN_MOVED_TO .. "   inmovedto")
+
   local wd = handle:addwatch(watch_dir, inotify.IN_CREATE, inotify.IN_MOVED_TO, inotify.IN_MOVE)
   while watch_active==true do
 
     local inotify_events = handle:read()
-    
+
     for i,ev in pairs(inotify_events) do
       if(ev["mask"] == inotify.IN_MOVED_TO) then
-          
+
         local ingest_file = watch_dir ..'/'.. ev.name
         print (inotify.IN_MOVED_TO.. " moved to")
         print (inotify.IN_MOVE .. " moved")
@@ -378,19 +420,17 @@ local function handle_read(watch_dir, button2)
         print (ev["mask"] == inotify.IN_MOVED_TO)
         print(ingest_file)
           --ingest_file = df.sanitize_filename(ingest_file) 
-          
+
         local filetype = df.get_filetype(ingest_file)
-        print(filetype .."filetype")  
+        print(filetype .."filetype")
           if table_contains(ext_watched_extensions,filetype) then
             print("file wanted")
-            imported_image = import_to_library(ingest_file)
-          if move_files.value == true then sort_in_library(imported_image) end
-        
+          local imported_image = import_to_library(ingest_file)
+          if move_files.value == true then sort_in_library(imported_image, i) end
+
            else
             print ("not that one")
-      end 
-
-      
+      end
       end
       end
   sleep(1500)
@@ -403,74 +443,77 @@ end
 
 
 
-local function buttonclicked(button, button2)
+local function start_capture(button_start_capture, button_stop_capture)
 
 
-    dt.print(_("button clicked"))
---    if watch_dir == nil then 
-  --  dt.print("please choose a directory to import from")
-    --return
-
---  end   
     local watch_dir = ingest_directory.value
-    button2.visible = true
-    button.visible = false
-    ingest_directory.visible = false
-    jobcode.visible = false
+
+    if watch_dir == nil then
+      dt.print("please choose a directory to import from")
+    return
+
+    end
+
+    button_stop_capture.visible = true
+    button_start_capture.visible = false
+    ingest_directory_box.visible = false
+    jobcode_box.visible = false
+    destination_directory_box.visible = false
+
     print("state1")
     print(watch_dir)
-    handle_read(watch_dir, button2)
+    handle_read(watch_dir)
     print("state2")
     print('done_daniel')
 end
 
-local function button2clicked(button, button2)
-    local watch_dir=ingest_directory.value
-    print ("button2 clicked")
-    dt.print("button2 clicked")
+local function stop_capture(button_start_capture, button_stop_capture)
+
+    dt.print("Capture Session Ended")
     watch_active = false
-    --handle:rmwatch(wd)
-    -- handle_read_cancel(watch_dir)
-    button2.visible =false
-    button.visible =true
-    ingest_directory.visible = true
-    jobcode.visible = true
+    button_stop_capture.visible =false
+    button_start_capture.visible =true
+    ingest_directory_box.visible = true
+    dt.preferences.write("Pseudo_Tether", "session_counter", "integer", dt.preferences.read("Pseudo_Tether", "session_counter", "integer")+1)
+    session_counter.label = dt.preferences.read("Pseudo_Tether", "session_counter", "integer")
+    move_files_extra_options()
 end
 
-  local button3 = dt.new_widget("button")
-      {label = "handle close"
-  }
+--  local button3 = dt.new_widget("button")
+ --     {label = "handle close"
+  --}
 
-local button = dt.new_widget("button")
+local button_start_capture = dt.new_widget("button")
         {
-          label = _("my button"),
+          label = _("Start Capture Session"),
         }
 
-local button2 = dt.new_widget("button")
+local button_stop_capture = dt.new_widget("button")
         {
-          label = _("my button2"),
+          label = _("End Capture Session"),
           visible = false,
         }
 dd.dprint(inotify)
-button.clicked_callback = function () buttonclicked(button, button2) end
-button2.clicked_callback = function() button2clicked(button, button2) end
-button3.clicked_callback = function() button3_clicked(handle_read_cancel) end
-  -- code
+
+move_files.clicked_callback = function() move_files_extra_options() end
+button_start_capture.clicked_callback = function () start_capture(button_start_capture,button_stop_capture) end
+button_stop_capture.clicked_callback = function() stop_capture(button_start_capture,button_stop_capture) end
+reset_session_counter.clicked_callback = function() dt.preferences.write("Pseudo_Tether", "session_counter", "integer", "0000") session_counter.label = 0000 end
 
 -- pack the widgets in a table for loading in the moidule
-table.insert(mE.widgets, button)
---table.insert(mE.widgets, check_button)
-table.insert(mE.widgets, button2)
-table.insert(mE.widgets, button3)
+table.insert(mE.widgets, button_start_capture)
+table.insert(mE.widgets, button_stop_capture)
+-- table.insert(mE.widgets, button3)
 --table.insert(mE.widgets, combobox)
-table.insert(mE.widgets, jobcode)
-table.insert(mE.widgets, ingest_directory)
-table.insert(mE.widgets, move_files)
-table.insert(mE.widgets, destination_directory)
+table.insert(mE.widgets, ingest_directory_box)
 
---table.insert(mE.widgets, label)i
---table.insert(mE.widgets, separator)
---table.insert(mE.widgets, slider)
+table.insert(mE.widgets, separator)
+
+table.insert(mE.widgets, move_files)
+table.insert(mE.widgets, destination_directory_box)
+table.insert(mE.widgets, jobcode_box)
+table.insert(mE.widgets, move_files_options)
+table.insert(mE.widgets, label)
 
 -- ... and tell dt about it all
 
